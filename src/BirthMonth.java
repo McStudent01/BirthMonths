812951c744ec39a93aff84901400df1c19667a95
McStudent01 <mcmillcy@mail.uc.edu>
2023-06-14 14:16:03 -0500

    Final Commit

--- src/BirthMonth.java
+++ src/BirthMonth.java
@@ -9,6 +9,12 @@ public class BirthMonth
         System.out.println("Please enter your birth month in number form [1-12]: ");
         birthMonth = in.nextInt();
 
-       
+        if (birthMonth >= 1 && birthMonth <= 12)
+        {
+            System.out.println("Your birth month is: " + birthMonth);
+        } else
+        {
+            System.out.println("You entered an incorrect month: " +birthMonth + "\nPlease try again.");
+        }
     }
 }
