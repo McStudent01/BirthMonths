995c41aaad8cc904701e406d34461f34cbe79b6a
McStudent01 <mcmillcy@mail.uc.edu>
2023-06-14 14:14:27 -0500

    Initial commit

---
+++ src/BirthMonth.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class BirthMonth
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int birthMonth = 0;
+
+        System.out.println("Please enter your birth month in number form [1-12]: ");
+        birthMonth = in.nextInt();
+
+       
+    }
+}
